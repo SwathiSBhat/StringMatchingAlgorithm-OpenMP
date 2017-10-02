07d09730ec1b804df70d031e825fd25cef9fda9e
swathi s bhat <swathibhat282@gmail.com>
2017-10-02 18:10:27 +0530

    Update bmh.c

--- bmh.c
+++ bmh.c
@@ -15,14 +15,8 @@ int len=strlen(str);
 for(i=0;i<len-1;i++)
 {
 a=str[i];
-//printf("str[%d]   %c (int)str[%d]=%d\n",i,str[i],i,(int)str[i]);
 occ[a]=i;
-//printf("occ[%c]=%d",a,occ[a]);
 }
-/*printf("occurence of str chars:\n");
-for(i=0;str[i]!='\0';i++)
-printf("%c----%d\n",str[i],occ[(int)str[i]]);
-*/
 return occ;
 }
 
@@ -41,16 +35,11 @@ while(i0<=(n-m))
     j--;
   if(j<0)
   {
- // printf("i0 = %d\n",i0);
   printf("Pattern found at %d\n",i0);
-
   }
-  //i0=i0+(m-1)-locc[t[i0+(m-1)]];
   i0+=m-1;
   i0-=locc[t[i0]];
-  //printf("i0=  %d\n",i0);
 }
-//return -1;
 }
 
 
@@ -58,21 +47,9 @@ int main()
 {
   char pat[10],text[256];
   int n,m;
-  /*printf("size of text: \n");
-  scanf("%d",&n);
-  printf("size of pattern:\n");
-  scanf("%d",&m);
-  printf("Enter text and pattern\n");
-*/
   scanf("%s",text);
   scanf("%s",pat);
   printf("text : %s pattern : %s\n",text,pat);
   bmh(text,pat);
-  /*int ans=bmh(text,pat);
-  if(ans>=0)
-  printf("Pattern found at index : %d\n",ans);
-  else
-  printf("Not found\n");
- */
  return 0;
 }
